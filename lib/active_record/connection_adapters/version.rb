08d2cd74080dff49effe430d49a79ab44e8c1a6d
Yaroslav Barkovskiy <y.barkovskij@unact.ru>
2019-04-18 11:25:47 +0300

    DEV-38428

--- lib/active_record/connection_adapters/version.rb
+++ lib/active_record/connection_adapters/version.rb
@@ -1,5 +1,5 @@
 module Activerecord
   module ConnectionAdapters
-    VERSION = "2.1.12"
+    VERSION = "2.1.13"
   end
 end
--- lib/arel/visitors/sqlanywhere.rb
+++ lib/arel/visitors/sqlanywhere.rb
@@ -4,55 +4,55 @@ module Arel
       private
 
       def visit_Arel_Nodes_SelectStatement(o, collector)
+        if o.with
+          collector = visit o.with, collector
+          collector << " "
+        end
 
-        # Use TOP x for limit statements
-        collector_limit = ActiveRecord::ConnectionAdapters::AbstractAdapter::SQLString.new
-        collector_limit = maybe_visit(o.limit, collector_limit)
-        # START AT x for offset
-        collector_offset = ActiveRecord::ConnectionAdapters::AbstractAdapter::SQLString.new
-        collector_offset = maybe_visit(o.offset, collector_offset)
-        # Add select
-        collector_select = ActiveRecord::ConnectionAdapters::AbstractAdapter::SQLString.new
-        o.cores.inject(collector_select) { |c,x|
+        if o.offset && !o.limit
+          o.limit = Arel::Nodes::Limit.new(2147483647)
+        end
+
+        if o.limit && o.orders.empty?
+          o.orders = [Arel::Nodes::Ascending.new(Arel.sql("1"))]
+        end
+
+        collector << "SELECT"
+
+        collector = maybe_visit o.limit, collector
+        collector = maybe_visit o.offset, collector
+
+        collector = o.cores.inject(collector) { |c,x|
           visit_Arel_Nodes_SelectCore(x, c)
         }
-        # Check for Arel distinct
-        using_distinct = o.cores.any? { |core|
-          core.set_quantifier.class == Arel::Nodes::Distinct
-        }
-        # Check for distinct in SQL statement
-        using_distinct = !(collector_select.value =~ /distinct/i).blank? if !using_distinct
-        # Create SQL statement
-        select_sql_without_order = [
-          "SELECT",
-          (" DISTINCT" if using_distinct),
-          (collector_limit.value if !collector_limit.value.blank?),
-          (visit(Arel::Nodes::Limit.new(2147483647)) if collector_limit.value.blank? && !collector_offset.value.blank?),
-          (collector_offset.value if !collector_offset.value.blank?),
-          (collector_select.value.sub(/^SELECT(\s+DISTINCT)?\s*/i, ' ')) # Remove SELECT and/or DISTINCT added by arel
-        ].compact.join('')
-        
-        collector << select_sql_without_order
-        # ORDER BY
-        collector = order_by_helper(o, collector)
-        collector = maybe_visit(o.lock, collector)
 
-        collector
-      end
-
-      def order_by_helper(o, collector)
-        if !o.orders.empty?
+        unless o.orders.empty?
           collector << ORDER_BY
           len = o.orders.length - 1
           o.orders.each_with_index { |x, i|
             collector = visit(x, collector)
             collector << COMMA unless len == i
           }
-        else
-          # Attempt to avoid SQLA error 'The result returned is non-deterministic'.
-          # Complete nonsense.
-          collector << " ORDER BY 1" if o.limit
         end
+
+        collector
+      end
+
+      def visit_Arel_Nodes_SelectCore(o, collector)
+        collector = maybe_visit o.set_quantifier, collector
+
+        collect_nodes_for o.projections, collector, " "
+
+        if o.source && !o.source.empty?
+          collector << " FROM "
+          collector = visit o.source, collector
+        end
+
+        collect_nodes_for o.wheres, collector, " WHERE ", " AND "
+        collect_nodes_for o.groups, collector, " GROUP BY "
+        collect_nodes_for o.havings, collector, " HAVING ", " AND "
+        collect_nodes_for o.windows, collector, " WINDOW "
+
         collector
       end
 
